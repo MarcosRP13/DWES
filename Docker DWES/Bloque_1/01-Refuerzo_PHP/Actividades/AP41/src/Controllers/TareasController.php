a5eb681f256da7a813c970c237477faadd2d1856
marope01 <marope01@alumnatflorida.es>
2025-10-24 09:03:08 +0200

    AP41- hecho

---
+++ Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP41/src/Controllers/TareasController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AP41\Controllers;
+use AP41\Core\EntityManager;
+use AP41\Entity\Tareas;
+use AP41\Views\DetalleTarea;
+use AP41\Views\ListadoTareas;
+use AP41\Views\FormularioView;
+/**
+ * Controlador para la ruta /detalle
+ */
+class TareasController
+{
+
+    public function list()
+    {
+        $entityManager = (new EntityManager())->getEntityManager();
+        $tareas = $entityManager->getRepository(Tareas::class);
+        $data = $tareas->findAll();
+        $view = new ListadoTareas($data);
+    }
+
+    public function detail($id = null)
+    {
+        $entityManager = (new EntityManager())->getEntityManager();
+        $tareas = $entityManager->getRepository(Tareas::class);
+        $data = $tareas->find($id);
+        $view = new DetalleTarea;
+        $this->render($data);
+    }
+}

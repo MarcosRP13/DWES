20cd1084c60fcca4019db51df4157ee93d269642
marope01 <marope01@alumnatflorida.es>
2025-12-10 08:28:30 +0100

    AP72 Crud creado

---
+++ Bloque3/AP71/src/Entity/Product.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100, nullable: false)]
+    #[Assert\NotBlank(message: "El nombre debe ser obligatorio.")]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: false)]
+    #[Assert\NotBlank(message: "El precio debe ser obligatorio")]
+    #[Assert\Positive(message: "El precio debe ser mayor que 0")]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    #[Assert\PositiveOrZero(message: "El stock debe ser mayor o igual a 0")]
+    private ?int $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
